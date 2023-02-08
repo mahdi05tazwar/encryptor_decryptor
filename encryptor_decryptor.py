046f96637ca07742f7c1d9f2cb1477799e06174a
mahdi05tazwar <83511272+mahdi05tazwar@users.noreply.github.com>
2023-02-08 22:10:03 +0600

    Add files via upload

---
+++ encryptor_decryptor.py
@@ -0,0 +1,26 @@
+class Encryptor_Decryptor():
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def Encrypt(self):
+        with open(self.filename, "r") as file:
+            filetxt = file.read()
+        txtlist = [*filetxt]
+        newstr = ""
+        for letter in txtlist:
+            newstr += chr(ord(letter)+ 3)
+        with open(self.filename, "w") as file:
+            file.write(newstr)
+
+    def Decrypt(self):
+        with open(self.filename, "r") as file:
+            filetxt = file.read()
+        txtlist = [*filetxt]
+        newstr = ""
+        for letter in txtlist:
+            newstr += chr(ord(letter)- 3)
+        with open(self.filename, "w") as file:
+            file.write(newstr)
+
+en1 = Encryptor_Decryptor("sample.txt")
+en1.Decrypt()
